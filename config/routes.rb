8ef5f840cb9e1693c1b97b3927613d9ad95f401d
root <root@localhost>
2023-02-10 23:36:01 +0000

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "main#index"
+
+  resources :users
+  get 'main', to: 'main#index'
+  
 end
