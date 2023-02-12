aeacb5919d7672d4e040093bc26ab77ad74709b5
root <root@localhost>
2023-02-12 06:06:00 +0000

    added alumni branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_02_11_143026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_143026) do
     t.string "user_id"
     t.string "grade"
     t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
