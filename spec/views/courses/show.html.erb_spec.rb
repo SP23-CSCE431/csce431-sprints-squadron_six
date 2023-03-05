7d20a7da06813864e1158444b868b2cc39bcbaf8
root <root@localhost>
2023-03-05 22:28:09 +0000

    some testing validation and simplecov

--- spec/views/courses/show.html.erb_spec.rb
+++ spec/views/courses/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "courses/show", type: :view do
   before(:each) do
-    assign(:course, Course.create!())
+    assign(:course, Course.create(course_name: "CSCE431", course_hours: 3))
   end
 
   it "renders attributes in <p>" do
