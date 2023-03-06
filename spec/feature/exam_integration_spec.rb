8f0128face695321639554d8d9a9ec6855ba3af2
root <root@localhost>
2023-03-06 18:16:21 +0000

    fixed integration failure

--- spec/feature/exam_integration_spec.rb
+++ spec/feature/exam_integration_spec.rb
@@ -6,12 +6,12 @@ require 'rails_helper'
 RSpec.describe 'Creating an Exam', type: :feature do
   # sunny day
   scenario 'valid inputs' do
+    Course.create(course_name:"CSCE_431", course_hours:3)
     visit new_exam_path
     fill_in "exam[exam_date]", with: Date.today + 1.week
-    fill_in "exam[exam_grade]", with: nil
-    fill_in "exam[course_id]", with: Course.create(course_name:"CSCE 431", course_hours:3).id
+    fill_in "exam[exam_grade]", with: 'A'
+    select "CSCE_431", :from =>"exam[course_id]"
     click_on 'Create Exam'
-    visit exams_path
     expect(page).to have_content('successfully created')
   end
 
