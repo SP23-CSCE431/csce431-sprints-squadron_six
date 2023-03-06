6829960aa6b29176975aab69d00e9f70d0d0f436
root <root@localhost>
2023-03-06 01:42:57 +0000

    testing added for alumni page

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -2,7 +2,11 @@ class Alumni < ApplicationRecord
   belongs_to :company
   belongs_to :user
 
-  validates_presence_of :company_id
-  validates_presence_of :user_id
-
+   # validate that all parameters are inputted
+   validates_presence_of:company_id
+   validates_presence_of:user_id
+   validates_presence_of:created_at
+   validates_presence_of:updated_at
+   # a course can't be created that already exists
+   validates_uniqueness_of:company_id
 end
