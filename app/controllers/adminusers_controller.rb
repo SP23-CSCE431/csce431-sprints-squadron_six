8057b58319c64f603c818c3db917b91b2d52f8e7
ivanattexas <ivanattexas@tamu.edu>
2023-04-04 13:53:47 -0500

    Locked Adminuser CRUD from non admins

--- app/controllers/adminusers_controller.rb
+++ app/controllers/adminusers_controller.rb
@@ -1,14 +1,21 @@
 class AdminusersController < ApplicationController
-  before_action :set_adminuser, only: %i[ show edit update destroy ]
+  before_action :set_adminuser, only: %i[show edit update destroy]
+  def checkaccess
+    return if isadmin
+
+    redirect_to new_authuser_session_path, alert: 'You are not authorized to access this page.'
+
+    # rest of the action code
+  end
 
   # GET /adminusers or /adminusers.json
   def index
+    checkaccess
     @adminusers = Adminuser.all
   end
 
   # GET /adminusers/1 or /adminusers/1.json
-  def show
-  end
+  def show; end
 
   # GET /adminusers/new
   def new
@@ -16,16 +23,17 @@ class AdminusersController < ApplicationController
   end
 
   # GET /adminusers/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /adminusers or /adminusers.json
   def create
+    return unless isadmin
+
     @adminuser = Adminuser.new(adminuser_params)
 
     respond_to do |format|
       if @adminuser.save
-        format.html { redirect_to adminuser_url(@adminuser), notice: "Adminuser was successfully created." }
+        format.html { redirect_to adminuser_url(@adminuser), notice: 'Adminuser was successfully created.' }
         format.json { render :show, status: :created, location: @adminuser }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -36,9 +44,11 @@ class AdminusersController < ApplicationController
 
   # PATCH/PUT /adminusers/1 or /adminusers/1.json
   def update
+    return unless isadmin
+
     respond_to do |format|
       if @adminuser.update(adminuser_params)
-        format.html { redirect_to adminuser_url(@adminuser), notice: "Adminuser was successfully updated." }
+        format.html { redirect_to adminuser_url(@adminuser), notice: 'Adminuser was successfully updated.' }
         format.json { render :show, status: :ok, location: @adminuser }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,22 +59,25 @@ class AdminusersController < ApplicationController
 
   # DELETE /adminusers/1 or /adminusers/1.json
   def destroy
+    return unless isadmin
+
     @adminuser.destroy
 
     respond_to do |format|
-      format.html { redirect_to adminusers_url, notice: "Adminuser was successfully destroyed." }
+      format.html { redirect_to adminusers_url, notice: 'Adminuser was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_adminuser
-      @adminuser = Adminuser.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def adminuser_params
-      params.require(:adminuser).permit(:email, :isaadmin)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_adminuser
+    @adminuser = Adminuser.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def adminuser_params
+    params.require(:adminuser).permit(:email, :isaadmin)
+  end
 end
