dafb48494e8ad37079bb22463d6739ef902c7c88
ivanattexas <ivanattexas@tamu.edu>
2023-04-06 12:10:45 -0500

    Added info oauth helper

--- spec/oauth_helper.rb
+++ spec/oauth_helper.rb
@@ -1,19 +1,19 @@
 def mock_google_oauth2
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-      provider: 'google_oauth2',
-      uid: '123456789',
-      info: {
-        email: 'user@example.com',
-        name: 'John Doe',
-        first_name: 'John',
-        last_name: 'Doe',
-        image: 'https://lh3.googleusercontent.com/a-/1234567890'
-      },
-      credentials: {
-        token: 'token',
-        refresh_token: 'refresh_token',
-        expires_at: Time.now + 1.week
-      }
-    })
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                       provider: 'google_oauth2',
+                                                                       uid: '108924254985749898215',
+                                                                       info: {
+                                                                         email: 'ivanattexas@tamu.edu',
+                                                                         name: 'Ivan Martinez',
+                                                                         first_name: 'Ivan',
+                                                                         last_name: 'Martinez',
+                                                                         image: 'https://lh3.googleusercontent.com/a/AGNmyxZ9w0R3_loFhlk46VAFMyqvC2ZqrZgHuPYnlaAH=s96-c'
+                                                                       },
+                                                                       credentials: {
+                                                                         token: auth.credentials.token,
+                                                                         refresh_token: auth.credentials.refresh_token,
+                                                                         expires_at: Time.now + 1.week
+                                                                       }
+                                                                     })
 end
