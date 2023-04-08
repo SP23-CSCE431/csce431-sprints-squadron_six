8ea9ff21d3237460ad8a20276ab3d8ddaa08637b
root <root@localhost>
2023-04-08 22:08:58 +0000

    search function alumni

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -7,4 +7,7 @@ class Alumni < ApplicationRecord
    validates_presence_of:user_id
    # a company can't be created that already exists
    # validates_uniqueness_of:company_id
+   def self.search(search)
+    where(['users.user_lname ILIKE ?', "%#{search}%"]).joins(:user)
+  end
 end
