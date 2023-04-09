e26d63d4401e5cdf0d1be1977bb9c23983ed9b9c
root <root@localhost>
2023-04-09 16:17:46 +0000

    alumni rspec functional

--- spec/routing/alumnis_routing_spec.rb
+++ spec/routing/alumnis_routing_spec.rb
@@ -33,5 +33,89 @@ RSpec.describe AlumnisController, type: :routing do
     it 'routes to #destroy' do
       expect(delete: '/alumnis/1').to route_to('alumnis#destroy', id: '1')
     end
+
+    context 'with search parameter' do
+      it 'routes to #index with search parameter' do
+        expect(get: '/alumnis?search=test').to route_to('alumnis#index', search: 'test')
+      end
+    end
+
+    context 'with invalid search parameter' do
+      it 'does not route to #index with invalid search parameter' do
+        expect(get: '/alumnis?invalid_param=test').not_to route_to('alumnis#index')
+      end
+    end
+
+    context 'as admin' do
+      it 'routes to #create' do
+        expect(post: '/alumnis', params: { isadmin: true }).to route_to('alumnis#create')
+      end
+
+      it 'routes to #update via PUT' do
+        expect(put: '/alumnis/1', params: { isadmin: true }).to route_to('alumnis#update', id: '1')
+      end
+
+      it 'routes to #update via PATCH' do
+        expect(patch: '/alumnis/1', params: { isadmin: true }).to route_to('alumnis#update', id: '1')
+      end
+
+      it 'routes to #destroy' do
+        expect(delete: '/alumnis/1', params: { isadmin: true }).to route_to('alumnis#destroy', id: '1')
+      end
+    end
+
+    # context 'as non-admin' do
+    #   it 'does not route to #create' do
+    #     expect(post: '/alumnis').not_to route_to('alumnis#create')
+    #   end
+
+    #   it 'does not route to #update via PUT' do
+    #     expect(put: '/alumnis/1').not_to route_to('alumnis#update', id: '1')
+    #   end
+
+    #   it 'does not route to #update via PATCH' do
+    #     expect(patch: '/alumnis/1', params: { isadmin: false }).not_to route_to('alumnis#update', id: '1')
+    #   end
+      
+    #   it 'does not route to #destroy' do
+    #     expect(delete: '/alumnis/1', params: { isadmin: false }).not_to route_to('alumnis#destroy', id: '1')
+    #   end
+    # end
+
+    # context 'as authenticated user' do
+    #   it 'routes to #edit' do
+    #     expect(get: '/alumnis/1/edit', session: { user_id: 1 }).to route_to('alumnis#edit', id: '1')
+    #   end
+
+    #   it 'does not route to #new' do
+    #    expect(get: '/alumnis/new', session: { user_id: 1 }).not_to route_to('alumnis#new')
+    #   end
+    # end
+
+    # context 'as unauthenticated user' do
+    #   it 'does not route to #new' do
+    #     expect(get: '/alumnis/new').not_to route_to('alumnis#new')
+    #   end
+
+    #   it 'does not route to #edit' do
+    #     expect(get: '/alumnis/1/edit').not_to route_to('alumnis#edit', id: '1')
+    #   end
+
+    #   it 'does not route to #create' do
+    #     expect(post: '/alumnis').not_to route_to('alumnis#create')
+    #   end
+
+    #   it 'does not route to #update via PUT' do
+    #     expect(put: '/alumnis/1').not_to route_to('alumnis#update', id: '1')
+    #   end
+
+    #   it 'does not route to #update via PATCH' do
+    #     expect(patch: '/alumnis/1').not_to route_to('alumnis#update', id: '1')
+    #   end
+
+    #   it 'does not route to #destroy' do
+    #     expect(delete: '/alumnis/1').not_to route_to('alumnis#destroy', id: '1')
+    #   end
+    # end
   end
 end
