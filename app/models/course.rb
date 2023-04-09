48a17bc87bdc5b2bb5ee743384f2255d53ca8ce5
root <root@localhost>
2023-04-09 15:23:49 +0000

    extra validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-    has_many :exam
+    has_many :exam, dependent: :destroy
     def self.ransackable_attributes(auth_object = nil)
         ["course_hours", "course_name"]
     end
@@ -7,10 +7,20 @@ class Course < ApplicationRecord
     validates_presence_of :course_name
     validates_presence_of :course_hours
 
-    #TODO: implement required format for course name & not allow delete if there are exams using the course
+    #TODO: implement required format for course name & 
 
-    validates_uniqueness_of :course_name
+    
 
     # course hours max=6, min=0. must be integer
     validates :course_hours, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 6}
+    validates :course_name, length: { is: 7 }, uniqueness: { case_sensitive: false }
+
+    validates_uniqueness_of :course_name
+
+    # makes sure deletion is cancelled if there are still exams
+    # before_destroy :check
+    # def check
+    # # will always be true since all grandchildren have already been destroyed at this stage
+    #     return self.exam.still_there.empty?
+    # end
 end
